4f2032f1d2c2f8b52a7f4e63c2b0bc299792e2a4
Nav <navyasuri@nyu.edu>
2019-10-26 16:48:40 -0400

    Pushing some boilerplate

--- flaskapp/main.py
+++ flaskapp/main.py
@@ -11,12 +11,26 @@ def show_page():
 @app.route('/other')
 def show_another():
     # get the auth portion of url
+    read_auth()
 
     # make a post to spotify auth
 
     # get the spotify auth related tokens
 
     # use location data to make the weather api call
+    keywords = get_weather_keyword(lat, lon)
 
-    # us weather data to create a spotify playlist 
+    # use weather data to create a spotify playlist
+    
+    # store the id of the playlist. use the playlist id to play on speaker
+    
     return "Hello"
+
+def get_weather_keyword(lat, lon):
+    pass
+    url = ""
+    params = {"lat": "", "lon": ""}
+    # res = requests.get(url, params=params)
+    # print(res.json)
+    summary = ""
+    return summary
